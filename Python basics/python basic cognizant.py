e64e541377acea3686ddacd2ddb77795fb1cbfe6
Anand-Raj-CSE <anandrajcode@gmail.com>
2022-03-15 12:55:41 +0530

    Added new knowledge of pyton through cognizant learn

---
+++ Python basics/python basic cognizant.py
@@ -0,0 +1,11 @@
+
+print(" This would print max 8 characters {0:2} and this would exclude first 2 characters {1:}".format('harekrishn','jaisiyaram'))
+
+# Allignment
+print("Left allignment {0:>8} || {0:10}".format('harekrishn','jaisiyaram'))
+
+
+
+# If I need only digits between any decimal places then we would write .f
+a=3.2589631485741258256686
+print("{0:>.8f}".format(a))
